23f42ab657f30432dc5a4f3ad92d60a4219026b1
yunahamazawa <yunahamazawa821@gmail.com>
2024-11-14 20:19:17 +0900

    Test追加

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,8 @@
 
 public class Test {
 	public static void main(String[] args) {
-		Persin taro = new Person();
-		taro.name=" 山田太郎" ;
+		Person taro = new Person();
+		taro.name=" 山田太郎";
 		taro.age=20;
 		
 		System.out.println(taro.name);
